81475ef975f8bd15dce2a214e6467e7128ebb36e
Vincent Roy <vincentroy8@gmail.com>
2012-01-03 20:56:20 -0400

    Add debug and output folder configs in the test file.

--- test.rb
+++ test.rb
@@ -7,5 +7,7 @@ face_config = JSON.parse( IO.read("face.json") )
 Songster.api_key = face_config["api_key"]
 Songster.api_secret = face_config["api_secret"]
 Songster.debug = true
+Songster.debug_folder = Pathname.new("images").expand_path
+Songster.output_folder = Pathname.new("images").expand_path
 
 Songster.generate!("images/me.jpg")
