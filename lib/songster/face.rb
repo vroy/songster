ef7e91ae557c7ed95e63703f5b7e2c82ed6c246e
Vincent Roy <vincentroy8@gmail.com>
2012-01-03 20:45:01 -0400

    Only create one Mouth object per face.

--- lib/songster/face.rb
+++ lib/songster/face.rb
@@ -3,20 +3,15 @@ module Songster
   # Provide access to the details of a Face found in Songster::Image
   class Face
 
-    attr_reader :tags, :photo
+    attr_reader :tags, :photo, :mouth
 
     # Setup the information required to make the API call to face.com.
     def initialize(tags, photo)
       @tags = tags
       @photo = photo
+      @mouth = Mouth.new(self)
     end
 
-    # Create a Mouth for self.
-    #
-    # @return [Array -> Mouth] information about the mouth that was found.
-    def mouth
-      Mouth.new(self)
-    end
   end # Face
 
 end # Songster
